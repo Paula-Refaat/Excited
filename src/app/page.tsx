c7e18056d3a8e3169755580287001df3b84ea795
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-10-09 13:52:52 +0300

    🐛 Fixing header width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Logo } from '@/components/shared/icons';
 const Home = () => {
   return (
     <div>
-      <header className="fixed left-0 top-0 z-50 flex items-center justify-end bg-background px-4 py-6 md:px-36">
+      <header className="fixed left-0 top-0 z-50 flex w-full items-center justify-end bg-background px-4 py-6 md:px-36">
         <Logo className="h-[14px] w-[150px] scale-90 md:scale-100" />
       </header>
       <Hero />
